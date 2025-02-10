d55d39c729e25a2fda1ca80a3b8cb3d0d6dbe42d
Evan <spanglem@uc.edu>
2025-02-09 21:11:34 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    double springCosts = 154.7;
+    double summerCosts = 168.9;
+    double fallCosts = 143.6;
+    double winterCosts = 120.9;
+    double totalCosts = springCosts + summerCosts + fallCosts + winterCosts;
+    System.out.println("The total yearly maintenance costs are: " + totalCosts);
+    }
+}
